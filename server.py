dfff03aaae8700e8f2ed5dd6623265e2c7aa6247
Artyom-prog <mongol201323@gmail.com>
2025-07-30 22:48:39 +0500

    Update server.py

--- server.py
+++ server.py
@@ -3,6 +3,7 @@ from flask_sqlalchemy import SQLAlchemy
 from werkzeug.security import generate_password_hash, check_password_hash
 from datetime import datetime, timedelta
 import os
+import json
 import jwt
 import logging
 import firebase_admin
